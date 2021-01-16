703c18b80ba20b1b91f8265a6659a6e826cc5cc3
tezeloguzhan <tezeloguzhann@gmail.com>
2021-01-17 01:01:33 +0300

    Student Class added.

---
+++ python_iteration1/Student.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, id, fname, lname, exp):
+        self.id = id
+        self.fname = fname
+        self.lname = lname
+        self.exp = exp
+        self.attended_polls = 0
+        self.correct_ans = 0
