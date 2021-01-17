899d4d6816ab62a5ce7e8443a9d904c3f5915c95
mumin-kocamaz <74527764+mumin-kocamaz@users.noreply.github.com>
2021-01-17 14:43:31 +0300

    ZoomPollAnalyzer features added

--- python_iteration1/ZoomPollAnalyzer.py
+++ python_iteration1/ZoomPollAnalyzer.py
@@ -35,3 +35,26 @@ class ZoomPollAnalyzer:
             answer_keys.append(answer_key)
             count += 1
         return answer_keys
+
+    def populate_students_list(self, directory):
+        students = []
+        for file_name in glob.iglob('{}/*.xls'.format(directory), recursive=True):
+            start = False
+            df = pandas.read_excel(file_name)
+            for i in df.itertuples():
+                arr = np.asarray(i)
+                cleaned_row = np.asarray([x for x in arr if str(x) != 'nan'])
+                if cleaned_row.size < 2:
+                    start = False
+                if start:
+                    if cleaned_row.size < 6:
+                        exp = " "
+                    else:
+                        exp = cleaned_row[5]
+                    st = Student(cleaned_row[2], self.utils.strip_accents(cleaned_row[3]),
+                                 self.utils.strip_accents(cleaned_row[4]),
+                                 exp)
+                    students.append(st)
+                if np.isin("Öğrenci No", cleaned_row):
+                    start = True
+        return students
