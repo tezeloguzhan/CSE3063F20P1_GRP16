d138cb84c138466bc62547b964a9987c9ee5c75c
emirkilic6 <emirkilic@gmail.com>
2021-01-17 14:25:38 +0300

    answerkey features added

--- python_iteration1/Answerkey.py
+++ python_iteration1/Answerkey.py
@@ -12,3 +12,17 @@ class Answerkey:
 
     def get_name(self):
         return self.name
+
+    def is_question_present(self, que):
+        check = 0
+        for q in que:
+            for question in self.question_list:
+                if question.get_question().split() == str(q.question).split():
+                    check += 1
+                    break
+        return check == len(que)
+
+    def get_answer(self, que):
+        for question in self.question_list:
+            if question.get_question() == que:
+                return question.get_answer()
