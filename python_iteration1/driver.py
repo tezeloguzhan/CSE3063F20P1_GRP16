18ad16fe88c7866a2dad2c9ddd7814bbd6d36c66
mumin-kocamaz <74527764+mumin-kocamaz@users.noreply.github.com>
2021-02-06 17:52:45 +0300

    bug fixed

--- python_iteration1/driver.py
+++ python_iteration1/driver.py
@@ -1,7 +1,6 @@
 from ZoomPollAnalyzer import ZoomPollAnalyzer
 from tkinter import Tk, Listbox, Label, Button, filedialog, ttk, Toplevel, PhotoImage, Canvas
-import shutil, os, pathlib
-import numpy as np
+import os
 import pandas as pd
 from PIL import Image, ImageTk
 import glob
@@ -17,7 +16,6 @@ class GUI:
         self.listee = Listbox(master, width=80, height=35)
         self.listee.place(x=50, y=140)
         self.header = Label(master, text="ZOOM POLL ANALYZER", width=30, height=2, font=30).place(x=500, y=50)
-
         self.buton4 = Button(master, text="Start", width=35, height=2, command=self.start).place(x=850, y=475)
         self.buton5 = Button(master, text="Show Result", width=35, height=2, command=self.newwindow).place(x=850, y=550)
         self.buton6 = Button(master, text="Show Histograms", width=35, height=2, command=self.imagewindow).place(x=850,
@@ -26,7 +24,7 @@ class GUI:
     def start(self):
         driver = ZoomPollAnalyzer("answer-keys-directory", "students-list-directory", "polls-directory", "output")
         driver.start()
-        self.names = glob.glob(os.getcwd() + "/output/**/.xlsx", recursive=True)
+        self.names = glob.glob(os.getcwd() + "/output/**/*.xlsx", recursive=True)
         for i in self.names:
             if "Poll" in i.split("/")[-1]:
                 self.listee.insert(0, i.split("/")[-1])
@@ -44,7 +42,6 @@ class GUI:
         rows = len(sheetData)
         tree = ttk.Treeview(self.window, columns=data, show=["headings"], selectmode='browse')
         tree.place(x=0, y=100)
-
         for heading in headings:
             heading = str(heading)
             tree.column(heading, width=120, anchor='center')
@@ -80,7 +77,7 @@ class GUI:
         self.showimage = Label(self.imagewindoww, image=self.photo)
         self.showimage.resized = self.photo
         self.showimage.place(x=50, y=100)
-        excel = self.png_dict[png_file].split(".png")[0] + ".xlsx"
+        excel = self.png_dict[png_file].split(".png")[0]+".xlsx"
         xls = pd.ExcelFile(excel)
         sheetData = pd.read_excel(xls, 'Sheet1')
         headings = sheetData.columns
@@ -88,7 +85,6 @@ class GUI:
         rows = len(sheetData)
         tree = ttk.Treeview(self.imagewindoww, columns=data, show=["headings"], selectmode='browse')
         tree.place(x=600, y=100)
-
         for heading in headings:
             heading = str(heading)
             tree.column(heading, width=200, anchor='center')
@@ -97,7 +93,6 @@ class GUI:
             rowvalue = sheetData.values[rownumber]
             rowdata = tuple(rowvalue)
             tree.insert('', 'end', values=rowdata)
-
 
 root = Tk()
 my_gui = GUI(root)
