be0241a537f8f756d7730d651345d5b5babc1bb8
SelahattinHusmen <selahattin.husmen@hotmail.com>
2021-02-06 16:32:37 +0300

    start added

--- python_iteration1/driver.py
+++ python_iteration1/driver.py
@@ -18,13 +18,18 @@ class GUI:
         self.listee = Listbox(master, width=80, height=35)
         self.listee.place(x=50, y=140)
         self.header = Label(master, text="ZOOM POLL ANALYZER", width=30, height=2, font=30).place(x=500, y=50)
-        self.buton1 = Button(master, text="Add Students List Files", width=35, height=2,
-                             command=self.addstudent_file).place(x=850, y=250)
-        self.buton2 = Button(master, text="Add Poll Files", width=35, height=2, command=self.addpoll_files).place(x=850,
-                                                                                                                  y=325)
-        self.buton3 = Button(master, text="Add Answers Files", width=35, height=2,
-                             command=self.addanswers_files).place(x=850, y=400)
+
         self.buton4 = Button(master, text="Start", width=35, height=2, command=self.start).place(x=850, y=475)
         self.buton5 = Button(master, text="Show Result", width=35, height=2, command=self.newwindow).place(x=850, y=550)
         self.buton6 = Button(master, text="Show Histograms", width=35, height=2, command=self.imagewindow).place(x=850,
                                                                                                                  y=625)
+
+    def start(self):
+        driver = ZoomPollAnalyzer("answer-keys-directory", "students-list-directory", "polls-directory", "output")
+        driver.start()
+        self.names = glob.glob(os.getcwd() + "/output/*/.xlsx", recursive=True)
+        for i in self.names:
+            if "Poll" in i.split("/")[-1]:
+                self.listee.insert(0, i.split("/")[-1])
+            elif "attendance" in i.split("/")[-1]:
+                self.listee.insert(0, i.split("/")[-1])
